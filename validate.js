904d629ff14ac2a050e7ddc0179d189e0ebad322
Tanngrisnr <fredrik.wintzell@gmail.com>
2020-09-17 17:02:33 +0200

    session 4, began form functionality. needs to be updated so the loop checks the different values more thorougly

---
+++ validate.js
@@ -0,0 +1,33 @@
+//https://www.w3schools.com/js/js_validation.asp form validation source/inspiration
+
+//initialize global variables
+let message = document.getElementById("message");
+let inputs = document.forms.contactForm.elements;
+//Validate that all inputs have been filled with the correct data
+function validateForm() {
+  //select form inputs
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "") {
+      message.textContent = `there are empty input fields`;
+      return false;
+    } else {
+      message.textContent = `ready to roll`;
+      return true;
+    }
+  }
+  /* let contactName = document.forms["contactForm"]["contactName"];
+  let subject = document.forms["contactForm"]["subject"];
+  let email = document.forms["contactForm"]["email"];
+
+  if (contactName.value === "" || subject.value === "" || email.value === "") {
+    console.log("there is a missing value");
+    message.textContent = "Error";
+    return false;
+  } else {
+    console.log("this input has a value");
+    return true;
+  }*/
+  //check input value
+  //return false if invalid
+  //return true if valid
+}
