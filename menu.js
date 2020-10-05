362fab19f2ec1ea7aaf7e112f93a08b626d24375
Tanngrisnr <fredrik.wintzell@gmail.com>
2020-10-05 22:05:19 +0200

    5/10/2020 added hamburger menu using checkbox trick

--- menu.js
+++
@@ -1,9 +0,0 @@
-//complete javascript for curtain menu source: https://www.w3schools.com/howto/howto_js_curtain_menu.asp
-
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
